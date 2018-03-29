ca3675bf0f12e46987312ed8a3a1043a9232a8b1
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-29 16:44:21 -0700

    file restructure

---
+++ challenges/shift-array/shift_array.py
@@ -0,0 +1,12 @@
+def insertShiftArray(arr, num):
+    answer = [0] * (len(arr) + 1)
+    middle = (len(arr) + len(arr) % 2) // 2
+
+    for item in range(len(answer)):
+        if item < middle:
+            answer[item] = arr[item]
+        elif item == middle:
+            answer[item] = num
+        else:
+            answer[item] = arr[item - 1]
+    return answer
