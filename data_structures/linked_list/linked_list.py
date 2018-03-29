703b180ea45f3adcf710846fa3609b9c49535873
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-28 22:29:48 -0700

    added more tests

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -31,32 +31,5 @@ class LinkedList:
         while current:
             if value == current.data:
                 return True
-            current = current._next
+            current = current.next
         return False
-
-    def append(self, value):
-        current = self.head
-        while current._next:
-            current = current._next
-        current._next = Node(value)
-
-    def insert_before(self, value, new_value):
-        current = self.head
-        while current._next != value:
-            current = current._next
-        current._next = Node(new_value, current._next)
-
-    def insert_after(self, value, new_value):
-        current = self.head
-        while current != value:
-            current._next = Node(new_value, current.next)
-
-    def ll_kth_from_end(self, value):
-        current = self.head
-        if len(self) - 1 < value or value < 0:
-            print('out of range')
-            return False
-        else:
-            for _ in range(value - 1):
-                current = current._next
-        return current
--- data_structures/linked_list/test_linked_list.py
+++ data_structures/linked_list/test_linked_list.py
@@ -14,6 +14,12 @@ def empty_linked_list():
 def more_filled_linked_list():
     return LinkedList([5, 6, 7, 8, 9 ,10, 15])
 
+def test_find_list(filled_linked_list):
+    assert filled_linked_list.find(1) == True
+    
+def test_empty_list(empty_linked_list):
+    assert empty_linked_list.find(4) == False
+
 def test_insert_into_empty_ll(empty_linked_list):
     empty_linked_list.insert(4)
     assert empty_linked_list.head.data == 4
@@ -21,3 +27,9 @@ def test_insert_into_empty_ll(empty_linked_list):
 def test_merge_lists(filled_linked_list, more_filled_linked_list):
     merge_lists(filled_linked_list, more_filled_linked_list)
     assert str(filled_linked_list) == str([1, 5, 2, 6, 3, 7, 8, 9, 10 ,15])
+
+def test_find_value(empty_linked_list):
+    empty_linked_list.insert(2)
+    assert empty_linked_list.head.data == 2
+
+
