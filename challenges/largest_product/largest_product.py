c8a81408f59796e1223382bc2ff9402815d3cff2
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-03-30 14:40:12 -0700

    commit before branch change

--- challenges/largest_product/largest_product.py
+++ challenges/largest_product/largest_product.py
@@ -1,8 +1,8 @@
-def largest_product():
+def largest_product(new_list):
     counter = 0
     answer = 0
     new_list = []
-    
+
     for item in new_list:
         new_list[counter] = item[0] * item[1]
     for answer in new_list:
--- challenges/largest_product/test_largest_product.py
+++ challenges/largest_product/test_largest_product.py
@@ -1,8 +1,12 @@
-import largest_product as lp
+import largest_product.largest_product as lp
+import pytest
 
-# def test_largest_product():
-#     assert lp.largest_product([[1, 2], [3, 4], [5, 6], [7, 8]]) == 56
-
+@pytest.fixture
+def arr():
+    return [[1, 2], [3, 4], [5, 6], [7, 8]]
 
 def test_largest_product_not():
-    assert lp.largest_product() == 0
+    assert lp.largest_product([0]) == 0
+
+def test_largest_product():
+    assert lp.largest_product(arr) == 56
