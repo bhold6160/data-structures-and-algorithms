1531945b60b250a44672830c4ee3ba71cee4ff0d
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-03 11:05:56 -0700

    fixed errors with indentation

--- queue-with-stacks/queue_with_stacks.py
+++ queue-with-stacks/queue_with_stacks.py
@@ -6,25 +6,25 @@ class Queue:
         self.stack_two = s()
         self._len = 0 
 
-    for item in iter:
-        self.enqueue(item)
+        for item in iter:
+            self.enqueue(item)
 
-def enqueue(self, value):
-    if value:
-        self.stack.push(value)
-        self._len += 1
-        return self.stack_one
+    def enqueue(self, value):
+        if value:
+            self.stack_one.push(value)
+            self._len += 1
+            return self.stack_one
 
-    return False
-
-def dequeue(self):
-    if self._len == 0:
         return False
-    else:
-        for _ in range(self._len - 2):
-            self.stack_two.push(self.stack.pop())
-            last = self.stack_one.pop()
-        for _ in range(self._len - 2):
-            self.stack_one.push(self.stack_two.pop())
-        self._len -= 1
-        return last
+
+    def dequeue(self):
+        if self._len == 0:
+            return False
+        else:    
+            for _ in range(self._len - 2):
+                self.stack_two.push(self.stack_two.pop())
+                last = self.stack_one.pop()
+            for _ in range(self._len - 2):
+                self.stack_one.push(self.stack_two.pop())
+            self._len -= 1
+            return last
