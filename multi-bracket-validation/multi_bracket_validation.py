6734bbbfc032e35afcf31e9ac40b24b4bf20ec7a
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-04 21:32:59 -0700

    working with tests passing

---
+++ multi-bracket-validation/multi_bracket_validation.py
@@ -0,0 +1,18 @@
+def multi_bracket_validation(user_input):
+    """
+    Validates if brackets have a matching pair
+    """
+    opening = ('(', '[', '{')
+    closing = (')', ']', '}')
+    bracket_dict = {opening[0]: closing[0],
+                    opening[1]: closing[1],
+                    opening[2]: closing[2]}
+    arr = []
+
+    for item in user_input:
+        if item in opening:
+            arr.append(bracket_dict[item])
+        elif item in closing:
+            if not arr or item != arr.pop():
+                return False
+    return not arr
