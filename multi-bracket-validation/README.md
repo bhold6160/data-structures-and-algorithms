b956f27fca2791d399fef8f491e2e95eb54529c0
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-04 22:13:12 -0700

    working with passing tests

---
+++ multi-bracket-validation/README.md
@@ -0,0 +1,8 @@
+# Multi-bracket Validation.
+<!-- Short summary or background information -->
+
+## Challenge
+<!-- Description of the challenge -->
+
+## Solution
+<!-- Embedded whiteboard image -->
--- multi-bracket-validation/multi_bracket_validation.py
+++ multi-bracket-validation/multi_bracket_validation.py
@@ -2,17 +2,18 @@ def multi_bracket_validation(user_input):
     """
     Validates if brackets have a matching pair
     """
-    opening = ('(', '[', '{')
-    closing = (')', ']', '}')
-    bracket_dict = {opening[0]: closing[0],
+    opening = ['(', '[', '{']
+    closing = [')', ']', '}']
+    brackets = {opening[0]: closing[0],
                     opening[1]: closing[1],
                     opening[2]: closing[2]}
     arr = []
 
     for item in user_input:
         if item in opening:
-            arr.append(bracket_dict[item])
+            arr.append(brackets[item])
         elif item in closing:
             if not arr or item != arr.pop():
                 return False
     return not arr
+
