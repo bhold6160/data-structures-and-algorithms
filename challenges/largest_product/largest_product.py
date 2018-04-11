10967492d046352c2dffd0459b2f159dbaa5cbba
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-11 13:09:58 -0700

    working and tests passing

--- challenges/largest_product/largest_product.py
+++ challenges/largest_product/largest_product.py
@@ -1,11 +1,6 @@
-def largest_product(new_list):
-    counter = 0
-    answer = 0
-    new_list = []
-
-    for item in new_list:
-        new_list[counter] = item[0] * item[1]
-    for answer in new_list:
-        if answer < item:
-            answer = item
-    return answer
+def largest_product(arr):
+    product = 0
+    for i in range(len(arr)):
+        if arr[i][0] * arr[i][1] > product:
+            product = arr[i][0] * arr[i][1]
+    return product
--- challenges/largest_product/test_largest_product.py
+++ challenges/largest_product/test_largest_product.py
@@ -1,12 +1,30 @@
-import largest_product.largest_product as lp
+from .largest_product import largest_product as lp
 import pytest
 
 @pytest.fixture
 def arr():
     return [[1, 2], [3, 4], [5, 6], [7, 8]]
 
-def test_largest_product_not():
-    assert lp.largest_product([0]) == 0
 
-def test_largest_product():
-    assert lp.largest_product(arr) == 56
+def test_largest_product_small(arr):
+    """
+    test for the largest product
+    """
+    assert lp(arr) == 56
+
+
+def test_largest_product_large():
+    """
+    test for the largest product
+    """
+    assert lp([[8, 20], [5, 4], [100, 100], [
+                           8, 2], [9, 3], [2, 20]]) == 10000
+
+
+def test_largest_product_medium():
+    """
+    test for the largest product
+    """
+    assert lp([[3, 8], [4, 10], [10, 50]]) == 500
+
+
