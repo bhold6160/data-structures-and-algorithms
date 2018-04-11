54081a32461f7bd8b214199ddbdec5dec3193ec2
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-11 14:42:31 -0700

    working with tests passing

--- find-max-value/test_find_max_value.py
+++ find-max-value/test_find_max_value.py
@@ -7,12 +7,21 @@ def full_tree():
     return bst([3, 8, 10, 35, 68, 4])
 
 def test_fmv_returns_max(full_tree):
+    """
+    checks the max value
+    """
     assert fmv(full_tree) == 68
 
 def test_fmv_returns_after_insert(full_tree):
+    """
+    Checks max value after new insertion
+    """
     full_tree.insert(70)
     assert fmv(full_tree) == 70
 
 def test_fmv_returns_correct(full_tree):
+    """
+    Checks max value persists after insertion
+    """
     full_tree.insert(9)
     assert fmv(full_tree) == 68
