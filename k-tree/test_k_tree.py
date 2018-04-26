8ce4ed9fb4893b8c836e74a353b274aec16a625a
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-25 18:33:52 -0700

    rewrote breadth first traversal and have two tests passing, will continue

--- k-tree/test_k_tree.py
+++ k-tree/test_k_tree.py
@@ -2,11 +2,11 @@ from k_tree import KTree as kt
 
 
 def test_single_insert(empty_tree):
+    """test insertion into empty tree"""
     empty_tree.insert(empty_tree.root, 4)
     assert empty_tree.root.val == 4
 
 
 def test_insert_in_filled_tree(filled_tree):
-    start_len = filled_tree._size
     filled_tree.insert(1, 17)
     assert filled_tree.root.val == 1
