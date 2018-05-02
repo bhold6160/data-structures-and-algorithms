d952826712538d445d3b35bff0b1c303eaef26b0
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-05-02 13:38:54 -0700

    fixed error with traversals

--- challenges/tree_intersection/tree_intersection.py
+++ challenges/tree_intersection/tree_intersection.py
@@ -2,15 +2,17 @@ from bst import Node, BST
 
 
 def tree_intersection(tree_one, tree_two):
+    b = BST()
+    n = Node()
     empty_set = set()
     set_one = set()
     set_two = set()
 
-    traversed_one = tree_one.pre_order(set_one.add(tree_one.val))
-    traversed_two = tree_two.pre_order(set_two.add(tree_one.val))
+    traversed_one = b.pre_order(set_one.add(tree_one))
+    traversed_two = b.pre_order(set_two.add(tree_one))
 
     for i in traversed_one:
         for j in traversed_two:
-            if j == traversed_one[i]:
-                empty_set.add(j)
+            if traversed_one[i] == traversed_two[j]:
+                empty_set.add(j.val)
     return empty_set
