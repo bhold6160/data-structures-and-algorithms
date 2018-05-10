65cc770d687f787d6008d6d383c6dbc4a0431eb1
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-05-10 13:58:38 -0700

    fixed indentation

--- challenges/radix_sort/radix_sort.py
+++ challenges/radix_sort/radix_sort.py
@@ -8,18 +8,18 @@ def radix_sort(lst):
         max_len = True
         buckets = [[] for _ in range(BASE)]
 
-    for i in lst:
-        current_val = i // val
-        buckets[current_val % BASE].append(i)
-        if max_len and current_val > 0:
-            max_len = False
+        for i in lst:
+            current_val = i // val
+            buckets[current_val % BASE].append(i)
+            if max_len and current_val > 0:
+                max_len = False
 
-    counter = 0
-    for current in range(BASE):
-        bucket = buckets[current]
-        for item in bucket:
-            lst[counter] = item
-            counter += 1
+        counter = 0
+        for current in range(BASE):
+            bucket = buckets[current]
+            for item in bucket:
+                lst[counter] = item
+                counter += 1
 
-    val *= BASE
+        val *= BASE
     return lst
